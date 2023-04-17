f9f696bcbdcb9e2b829b866c50958cd3c8bfd523
umi2002 <jonathanhoang2002@gmail.com>
2023-04-17 16:35:24 -0400

    .

--- projet/build/CMakeFiles/Chess_couverture.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Chess_couverture"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Chess_couverture.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- projet/build/CMakeFiles/Chess_couverture.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Chess_couverture.
-# This may be replaced when dependencies are built.
